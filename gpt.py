ddf9b9c119cf0305cfcdb56d970e8ef4e5ef73f7
MarcelChew1 <72686386+MarcelChew1@users.noreply.github.com>
2024-06-15 00:51:17 +1000

    gpt

--- gpt.py
+++ gpt.py
@@ -6,12 +6,13 @@ batch_size = 64
 block_size = 256
 max_iters = 5000
 eval_interval = 500
-learning_rate = 1e-2
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+learning_rate = 3e-4
+device = 'cuda' #if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embd = 50
-n_head = 1
+n_embd = 384
+n_head = 6
 n_layer = 6
+dropout = 0.2
 
 # !wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
 with open('input.txt', 'r', encoding='utf-8') as f:
@@ -52,14 +53,42 @@ def estimate_loss():
     model.train()
     return out
 
+class EncapsulatedMultiHeadAttention(nn.Module):
+  def __init__(self):
+    super().__init__()
+    self.attn_attr = nn.Linear(n_embd, n_embd * 3)
+    self.proj = nn.Linear(n_embd, n_embd)
+    self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+    self.dropout = nn.Dropout(dropout)
+
+  def forward(self, x):
+    B, T, C = x.shape
+    q, k, v = self.attn_attr(x).split(n_embd, dim=2) # keeps dimensions B, T from input, final dim (2) becomes 3 * n_embd composed of q,k,v which are n_heads * head_size in dimension
+    
+    q = q.view(B, T, n_head, C // n_head).transpose(1, 2) # initially B, T, C -> B, T, heads, head_size transpose so the num heads is a batch dimension
+    k = k.view(B, T, n_head, C // n_head).transpose(1, 2)
+    v = v.view(B, T, n_head, C // n_head).transpose(1, 2)
+
+    attn = q @ k.transpose(-2, -1)*k.shape[-1]**-0.5 # B, nh, T, hs @ B, nh, hs, T => B, nh, T, T
+    attn = attn.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # adds batch dim 1, 1 and projects
+    attn = F.softmax(attn, dim=-1)
+    attn = self.dropout(attn)
+    y = attn @ v # B, nh, T, hs
+    
+    y = y.transpose(1, 2).contiguous().view(B, T, C) # reorder back to original structure heads move to dim = 2
+    y = self.dropout(self.proj(y)) # output projection layer
+
+    return y
 class MultiHeadAttention(nn.Module):
   def __init__(self, n_head, head_size):
     super().__init__()
     self.heads = nn.ModuleList([Head(head_size) for _ in range(n_head)])
     self.proj = nn.Linear(head_size * n_head, n_embd)
+    self.dropout = nn.Dropout(dropout)
 
   def forward(self, x):
     out = torch.cat([h(x) for h in self.heads], dim=-1)
+    out = self.dropout(self.proj(out))
     return out
 
 class Head(nn.Module):
@@ -70,6 +99,8 @@ class Head(nn.Module):
     self.value = nn.Linear(n_embd, head_size, bias=False)
     self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
 
+    self.dropout = nn.Dropout(dropout)
+
   def forward(self, x):
     B, T, C = x.shape
     k = self.key(x)
@@ -77,6 +108,7 @@ class Head(nn.Module):
     wei = q @ k.transpose(-2, -1)*k.shape[-1]**-0.5 # B, T, head_size * B, head_size, T = B, T, T
     wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf'))
     wei = F.softmax(wei, dim=-1)
+    wei = self.dropout(wei)
     v = self.value(x)
     out = wei @ v
     return out
@@ -85,10 +117,12 @@ class FeedForward(nn.Module):
   def __init__(self, n_embd):
     super().__init__()
     self.net = nn.Sequential(
-        nn.Linear(n_embd, 4 * n_embd),
-        nn.ReLU(),
-        nn.Linear(4 * n_embd, n_embd)
+      nn.Linear(n_embd, 4 * n_embd),
+      nn.ReLU(),
+      nn.Linear(4 * n_embd, n_embd),
+      nn.Dropout(dropout),
     )
+    
   def forward(self, x):
     return self.net(x)
   
@@ -96,7 +130,8 @@ class Block(nn.Module):
   def __init__(self, n_embd, n_head):
     super().__init__()
     head_size = n_embd // n_head
-    self.sa = MultiHeadAttention(n_head, head_size)
+    # self.sa = MultiHeadAttention(n_head, head_size)
+    self.sa = EncapsulatedMultiHeadAttention()
     self.ffwd = FeedForward(n_embd)
     self.ln1 = nn.LayerNorm(n_embd)
     self.ln2 = nn.LayerNorm(n_embd)
@@ -115,6 +150,20 @@ class GPTLanguageModel(nn.Module):
     self.block = nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range(n_layer)])
     self.ln_f = nn.LayerNorm(n_embd)
     self.lm_head = nn.Linear(n_embd, vocab_size)
+    
+    self.apply(self._init_weights)
+
+  def _init_weights(self, module):
+      if isinstance(module, nn.Linear):
+        nn.init.normal_(module.weight, mean=0.0, std=0.02)
+        # nn.init.kaiming_uniform_(module.weight)
+        if module.bias is not None:
+          nn.init.zeros_(module.bias)
+      elif isinstance(module, nn.Embedding):
+        nn.init.normal_(module.weight, mean=0, std=0.02)
+      elif isinstance(module, nn.LayerNorm):
+        nn.init.ones_(module.weight)
+        nn.init.zeros_(module.bias)
 
   def forward(self, idx, targets=None):
     B, T = idx.shape
@@ -137,39 +186,57 @@ class GPTLanguageModel(nn.Module):
 
   def generate(self, idx, max_new_tokens):
     for _ in range(max_new_tokens):
-      logits, loss = self(idx)
+      idx_cond = idx[:, -block_size:]
+      logits, loss = self(idx_cond)
       logits = logits[:, -1, :]
       probs = F.softmax(logits, dim=-1)
       idx_next = torch.multinomial(probs, num_samples=1)
       idx = torch.cat((idx, idx_next), dim=1)
-      print(idx)
     return idx
   
+
+def train_model():
+  # print the number of parameters in the model
+  print(sum(p.numel() for p in m.parameters())/1e6, 'M parameters')
+
+  # create a PyTorch optimizer
+  optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+
+  for iter in range(max_iters):
+
+      # every once in a while evaluate the loss on train and val sets
+      if iter % eval_interval == 0 or iter == max_iters - 1:
+          losses = estimate_loss()
+          print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+
+      # sample a batch of data
+      xb, yb = get_batch('train')
+
+      # evaluate the loss
+      logits, loss = model(xb, yb)
+      optimizer.zero_grad(set_to_none=True)
+      loss.backward()
+      optimizer.step()
+
 model = GPTLanguageModel()
 m = model.to(device)
-# print the number of parameters in the model
-print(sum(p.numel() for p in m.parameters())/1e6, 'M parameters')
 
-# create a PyTorch optimizer
-optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+def save_model(model, path='model.pth'):
+    torch.save(model.state_dict(), path)
 
-for iter in range(max_iters):
+train_model()
+save_model(m)
 
-    # every once in a while evaluate the loss on train and val sets
-    if iter % eval_interval == 0 or iter == max_iters - 1:
-        losses = estimate_loss()
-        print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+def load_model(path='model.pth'):
+    model = GPTLanguageModel()
+    model.load_state_dict(torch.load(path))
+    model.to(device)
+    model.eval()
+    return model
 
-    # sample a batch of data
-    xb, yb = get_batch('train')
-
-    # evaluate the loss
-    logits, loss = model(xb, yb)
-    optimizer.zero_grad(set_to_none=True)
-    loss.backward()
-    optimizer.step()
+# m = load_model()
 
 # generate from the model
 context = torch.zeros((1, 1), dtype=torch.long, device=device)
-print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
+print(decode(m.generate(context, max_new_tokens=5000)[0].tolist()))
 #open('more.txt', 'w').write(decode(m.generate(context, max_new_tokens=10000)[0].tolist()))
